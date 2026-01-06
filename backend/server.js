9f14706eb1cbe9ba980086709b07abc552b0a605
mohankumaronly <mohankumaronly81@gmail.com>
2026-01-06 22:11:33 +0530

    refactor the post in the server file

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const startServer = async () => {
   try {
     await databaseConnection(process.env.MONGODB_URL);
 
-    const server = app.listen(PORT, () => {
+    const server = app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
 
